299d94538d09dfdd453b4cdee587864be1415e90
Greg <rolwesg@gmail.com>
2020-08-01 17:12:24 -0500

    I'm color blind

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 //color palette
-const lightGreen = Color.fromRGBO(251, 243, 225, 1);
+const yellow = Color.fromRGBO(251, 243, 225, 1);
 const green =  Color.fromRGBO(233, 138, 28, 1);
 const darkGreen = Color.fromRGBO(215, 121, 5, 1);
 
-const backgroundColor = lightGreen;
+const backgroundColor = yellow;
 const headerColor = green;
 const submitButtonColor = green;
 const headerIconColor = Colors.white;
@@ -15,9 +15,9 @@ const errorText = TextStyle(color: Colors.red, fontSize: 14.0);
 const headerText = TextStyle(color: Colors.white,);
 
 const emailInputDecoration = InputDecoration(
-                  hintText: 'Email',
-                );
+  hintText: 'Email',
+);
 
 const passwordInputDecoration = InputDecoration(
-                  hintText: 'Password',
-                );
+  hintText: 'Password',
+);
