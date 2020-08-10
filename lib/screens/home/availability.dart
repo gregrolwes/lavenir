95b8c56afd15a8daa7efb34fa0c13a340af5e903
Sai-56 <bsaishreyas@gmail.com>
2020-08-10 15:04:41 -0500

    day buttons are working

--- lib/screens/home/availability.dart
+++ lib/screens/home/availability.dart
@@ -10,12 +10,28 @@ class Availability extends StatefulWidget {
 }
 
 class _AvailabilityState extends State<Availability> {
-  @override
-  List nums = [1, 2, 3, 4, 5];
+  List<String> nums = <String>["1", "2", "3", "4", "5"];
 
   int index = 0;
-
+  String s = "";
+  @override
   Widget build(BuildContext context) {
+    Widget _DayButton(String day, int ind) {
+      return Container(
+        width: 125,
+        height: 200,
+        child: FlatButton(
+            color: Colors.orange,
+            onPressed: () {
+              setState(() {
+                index = ind;
+                s = nums.elementAt(index);
+              });
+            },
+            child: Text(day)),
+      );
+    }
+
     return Scaffold(
       body: Column(
         children: [
@@ -25,50 +41,29 @@ class _AvailabilityState extends State<Availability> {
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: <Widget>[
-                _DayButton("Monday"),
+                _DayButton("Monday", 0),
                 SizedBox(
                   width: 20,
                 ),
-                _DayButton("Tuesday"),
+                _DayButton("Tuesday", 1),
                 SizedBox(
                   width: 20,
                 ),
-                _DayButton("Wednesday"),
+                _DayButton("Wednesday", 2),
                 SizedBox(
                   width: 20,
                 ),
-                _DayButton("Thursday"),
+                _DayButton("Thursday", 3),
                 SizedBox(
                   width: 20,
                 ),
-                _DayButton("Friday"),
+                _DayButton("Friday", 4),
               ],
             ),
           ),
-          // Text("j")
+          Container(child: Text("$s")),
         ],
       ),
-    );
-  }
-}
-
-class _DayButton extends StatefulWidget {
-  String day = "";
-  _DayButton(String day) {
-    this.day = day;
-  }
-  @override
-  __DayButtonState createState() => __DayButtonState();
-}
-
-class __DayButtonState extends State<_DayButton> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 125,
-      height: 200,
-      child: FlatButton(
-          color: Colors.orange, onPressed: () {}, child: Text(widget.day)),
     );
   }
 }
