827560018e97023bde1a37003003392bed9bcfa5
Sai-56 <bsaishreyas@gmail.com>
2020-08-11 15:31:26 -0500

    UI adjustments

--- lib/screens/home/availability.dart
+++ lib/screens/home/availability.dart
@@ -8,14 +8,21 @@ class Availability extends StatefulWidget {
 class _AvailabilityState extends State<Availability> {
   Map m = {
     'Monday': [
-      Sessions("4 -5", true),
-      Sessions("4 -5", true),
-      Sessions("4 -5", true)
+      Sessions("4 - 5 pm", true),
+      Sessions("5 - 6 pm", false),
+      Sessions("6 - 7 pm", true)
     ],
-    'Tuesday': [Sessions("4 -5", true)],
-    'Wednesday': [Sessions("4 -5", true)],
-    'Thursday': [Sessions("4 -5", true)],
-    'Friday': [Sessions("4 -5", true)]
+    'Tuesday': [Sessions("3 - 4 pm", true), Sessions("6 - 7 pm", false)],
+    'Wednesday': [Sessions("4 - 5 pm", true), Sessions("6 - 7 pm", false)],
+    'Thursday': [
+      Sessions("4 -5 pm", true),
+      Sessions("6 - 7 pm", false),
+      Sessions("7 - 8 pm", false),
+      Sessions("8 - 9 pm", false)
+    ],
+    'Friday': [],
+    'Saturday': [],
+    'Sunday': []
   };
 
   Map indToDay = {
@@ -23,21 +30,23 @@ class _AvailabilityState extends State<Availability> {
     1: "Tuesday",
     2: "Wednesday",
     3: "Thursday",
-    4: "Friday"
+    4: "Friday",
+    5: 'Saturday',
+    6: 'Sunday'
   };
 
   int _index = 0;
   String s = "";
 
   Widget Card(String slot, bool availbl, int index) {
-    slot = (availbl) ? slot + "\nAvailable" : slot + "\nUnavailable";
+    slot = (availbl) ? slot + "\n\nAvailable" : slot + "\n\nUnavailable";
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
       child: Container(
           width: 300,
           height: 150,
           child: FlatButton(
-            child: Text(slot),
+            child: Center(child: Text(slot)),
             color: (availbl) ? Colors.green : Colors.red,
             onPressed: () {
               setState(() {
@@ -52,57 +61,48 @@ class _AvailabilityState extends State<Availability> {
   @override
   Widget build(BuildContext context) {
     Widget _DayButton(String day, int ind) {
-      return Container(
+      return SizedBox(
+        height: 10,
         width: 125,
-        height: 200,
-        child: FlatButton(
-            color: Colors.orange,
-            onPressed: () {
-              setState(() {
-                _index = ind;
-              });
-            },
-            child: Text(day)),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 3, 5, 0),
+          child: FlatButton(
+              color: Colors.orange,
+              onPressed: () {
+                setState(() {
+                  _index = ind;
+                });
+              },
+              child: Text(day)),
+        ),
       );
     }
 
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 20.0),
-            height: 200.0,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: <Widget>[
-                _DayButton("Monday", 0),
-                SizedBox(
-                  width: 20,
-                ),
-                _DayButton("Tuesday", 1),
-                SizedBox(
-                  width: 20,
-                ),
-                _DayButton("Wednesday", 2),
-                SizedBox(
-                  width: 20,
-                ),
-                _DayButton("Thursday", 3),
-                SizedBox(
-                  width: 20,
-                ),
-                _DayButton("Friday", 4),
-              ],
-            ),
-          ),
           Expanded(
+              flex: 1,
+              child: Text(
+                indToDay[_index],
+                style: (TextStyle(fontSize: 40)),
+              )),
+          Expanded(
+              flex: 8,
               child: ListView.builder(
                   itemCount: m[indToDay[_index]].length,
                   itemBuilder: (BuildContext ctxt, int index) {
                     return Card(m[indToDay[_index]][index].slot,
                         m[indToDay[_index]][index].availbl, index);
-                  }))
-          // child: Text(m["Monday"][0].slot)),
+                  })),
+          Expanded(
+              flex: 3,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: m.keys.length,
+                  itemBuilder: (BuildContext ctxt, int index) {
+                    return _DayButton(indToDay[index], index);
+                  })),
         ],
       ),
     );
