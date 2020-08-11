af058092c0aab01f7c01de98ff9785e972a4c077
Sai-56 <bsaishreyas@gmail.com>
2020-08-11 14:42:18 -0500

    Added time slot cards

--- lib/screens/home/availability.dart
+++ lib/screens/home/availability.dart
@@ -6,10 +6,49 @@ class Availability extends StatefulWidget {
 }
 
 class _AvailabilityState extends State<Availability> {
-  List<String> nums = <String>["1", "2", "3", "4", "5"];
+  Map m = {
+    'Monday': [
+      Sessions("4 -5", true),
+      Sessions("4 -5", true),
+      Sessions("4 -5", true)
+    ],
+    'Tuesday': [Sessions("4 -5", true)],
+    'Wednesday': [Sessions("4 -5", true)],
+    'Thursday': [Sessions("4 -5", true)],
+    'Friday': [Sessions("4 -5", true)]
+  };
 
-  int index = 0;
+  Map indToDay = {
+    0: "Monday",
+    1: "Tuesday",
+    2: "Wednesday",
+    3: "Thursday",
+    4: "Friday"
+  };
+
+  int _index = 0;
   String s = "";
+
+  Widget Card(String slot, bool availbl, int index) {
+    slot = (availbl) ? slot + "\nAvailable" : slot + "\nUnavailable";
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+          width: 300,
+          height: 150,
+          child: FlatButton(
+            child: Text(slot),
+            color: (availbl) ? Colors.green : Colors.red,
+            onPressed: () {
+              setState(() {
+                availbl = (!availbl);
+                m[indToDay[_index]][index].availbl = availbl;
+              });
+            },
+          )),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget _DayButton(String day, int ind) {
@@ -20,8 +59,7 @@ class _AvailabilityState extends State<Availability> {
             color: Colors.orange,
             onPressed: () {
               setState(() {
-                index = ind;
-                s = nums.elementAt(index);
+                _index = ind;
               });
             },
             child: Text(day)),
@@ -57,9 +95,25 @@ class _AvailabilityState extends State<Availability> {
               ],
             ),
           ),
-          Container(child: Text("$s")),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: m[indToDay[_index]].length,
+                  itemBuilder: (BuildContext ctxt, int index) {
+                    return Card(m[indToDay[_index]][index].slot,
+                        m[indToDay[_index]][index].availbl, index);
+                  }))
+          // child: Text(m["Monday"][0].slot)),
         ],
       ),
     );
+  }
+}
+
+class Sessions {
+  String slot = "";
+  bool availbl = false;
+  Sessions(String slot, bool availbl) {
+    this.slot = slot;
+    this.availbl = availbl;
   }
 }
