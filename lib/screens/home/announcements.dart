a1bc7008be300409175820c68a27bd451a31bae4
k2khan <hamad.khan@slu.edu>
2020-08-29 09:14:37 -0700

    Updated Announcements Page

--- lib/screens/home/announcements.dart
+++ lib/screens/home/announcements.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'AnnouncementInfo.dart';
 
 class Announcements extends StatelessWidget {
+
+  final List<AnnouncementInfo> announcementList = [
+    AnnouncementInfo("Club Meeting This Friday", "We'll be meeting this Friday at 3pm over Zoom. The Zoom link will be emailed to you on Friday morning.", DateTime.now()),
+    AnnouncementInfo("Week Schedule Posted", "The schedule for next week is posted on the website. ", DateTime.now()),
+    AnnouncementInfo("New Time Slots", "We would like to announce that there are new time slots available. We are hoping that some tutors will fill in for those times!", DateTime.now()),
+    AnnouncementInfo("Week Schedule Posted", "The schedule for next week is posted on the website. ", DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Announcements'),
+    return Container(
+      child: new ListView.builder(
+        itemCount: announcementList.length,
+        itemBuilder: (BuildContext context, int index) => buildAnnouncementCard(context, index)
+      ), 
     );
   }
+
+  Widget buildAnnouncementCard(BuildContext context, int index) {
+    final announcement = announcementList[index];
+    return new Container(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(announcement.title, style: new TextStyle(fontSize: 25.0),),
+                    Spacer(),
+                    Icon(Icons.announcement, color: Colors.redAccent)
+                  ],),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 16.0),
+                child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: new Text(announcement.description)
+                         ,),
+                    ],),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                child: Row(
+                  children: <Widget>[
+                  Text(DateFormat('dd/MM/yyyy').format(announcement.postDate).toString()),
+                  Spacer(),
+                ],),
+              )
+            ]
+          ),
+        )
+      )
+    ); 
+  }
 }
+
--- lib/screens/home/availability.dart
+++ lib/screens/home/availability.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:lavenir/shared/constants.dart';
+
 class Availability extends StatefulWidget {
   @override
   _AvailabilityState createState() => _AvailabilityState();
@@ -38,11 +40,16 @@ class _AvailabilityState extends State<Availability> {
   int _index = 0;
   String s = "";
 
+<<<<<<< Updated upstream
   Widget Card(String slot, bool availbl, int index) {
     slot = (availbl) ? slot + "\n\nAvailable" : slot + "\n\nUnavailable";
+=======
+  Widget card(String slot, bool availble, int index) {
+>>>>>>> Stashed changes
     return Padding(
       padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
       child: Container(
+<<<<<<< Updated upstream
           width: 300,
           height: 150,
           child: FlatButton(
@@ -58,6 +65,84 @@ class _AvailabilityState extends State<Availability> {
     );
   }
 
+=======
+        margin: EdgeInsets.only(left: 10, top: 5, right: 10, bottom: 5),
+        height: MediaQuery.of(context).size.height / 8,
+        child: FlatButton(
+          child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: slot,
+                        style: TextStyle(
+                            fontSize:
+                                MediaQuery.of(context).size.height / 40)),
+                    TextSpan(
+                        text: (availble) ? "\n\nAvailable" : "\n\nUnavailable",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: (availble) ? Colors.greenAccent[700] : Colors.red[500],
+                            fontSize: MediaQuery.of(context).size.height / 38)
+                        ),
+                  ])),
+          onPressed: () {
+            setState(() {
+              availble = (!availble);
+              m[indToDay[_index]][index].availble = availble;
+            });
+          },
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              spreadRadius: 3,
+              blurRadius: 5,
+              offset: Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _dayButton(String day, int ind) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width / 4,
+      child: Padding(
+        // padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+        padding: (ind == _index)
+            ? EdgeInsets.fromLTRB(0, MediaQuery.of(context).size.height / 180,
+                MediaQuery.of(context).size.width / 80, 0)
+            : EdgeInsets.fromLTRB(0, MediaQuery.of(context).size.height / 80,
+                MediaQuery.of(context).size.width / 80, 0),
+        child: Opacity(
+          opacity: (ind == _index) ? 1.0 : 0.7,
+          child: FlatButton(
+            color: mainColor,
+            onPressed: () {
+              setState(() {
+                _index = ind;
+              });
+            },
+            child: Text(
+              day,
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height / 37,
+                color: Colors.black87,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+>>>>>>> Stashed changes
   @override
   Widget build(BuildContext context) {
     Widget _DayButton(String day, int ind) {
@@ -82,6 +167,7 @@ class _AvailabilityState extends State<Availability> {
       body: Column(
         children: [
           Expanded(
+<<<<<<< Updated upstream
               flex: 1,
               child: Text(
                 indToDay[_index],
@@ -103,6 +189,44 @@ class _AvailabilityState extends State<Availability> {
                   itemBuilder: (BuildContext ctxt, int index) {
                     return _DayButton(indToDay[index], index);
                   })),
+=======
+              flex: 3,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Text(
+                    indToDay[_index],
+                    style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.height / 20),
+                  ),
+                ),
+              ),
+          ),
+          Expanded(
+            flex: 16,
+            child: ListView.builder(
+              itemCount: m[indToDay[_index]].length,
+              itemBuilder: (BuildContext ctxt, int index) {
+                return card(m[indToDay[_index]][index].slot,
+                    m[indToDay[_index]][index].availble, index);
+              }
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: days.length,
+                itemBuilder: (BuildContext ctxt, int index) {
+                  return _dayButton(days[index], index);
+                }
+              ),
+            ),
+          ),
+>>>>>>> Stashed changes
         ],
       ),
     );
@@ -111,9 +235,9 @@ class _AvailabilityState extends State<Availability> {
 
 class Sessions {
   String slot = "";
-  bool availbl = false;
-  Sessions(String slot, bool availbl) {
+  bool availble = false;
+  Sessions(String slot, bool availble) {
     this.slot = slot;
-    this.availbl = availbl;
+    this.availble = availble;
   }
 }
