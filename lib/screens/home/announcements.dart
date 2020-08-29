87afdd6be52466188f522a64cd60fec29c1c2166
Greg <rolwesg@gmail.com>
2020-08-29 11:35:27 -0500

    Fixed date format on announcement cards

--- lib/screens/home/announcements.dart
+++ lib/screens/home/announcements.dart
@@ -51,7 +51,7 @@ class Announcements extends StatelessWidget {
               padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
               child: Row(
                 children: <Widget>[
-                Text(DateFormat('dd/MM/yyyy').format(announcement.postDate).toString()),
+                Text(DateFormat('MM/dd/yyyy').format(announcement.postDate).toString()),
                 Spacer(),
               ],),
             )
